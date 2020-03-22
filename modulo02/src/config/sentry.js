bec3b176ba7f5bbacf05df2eb434d77701c91ac3
Lucas <salleslucas00@gmail.com>
2020-03-22 00:52:50 -0300

    projeto finalizado

---
+++ modulo02/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://af10054fd9a745c4b31d66076d489337@sentry.io/2772737',
+};
---
+++ modulo04/src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './app.css';
+
+import TechList from './components/TechList';
+
+
+function App() {
+  return <TechList />
+}
+
+export default App;
